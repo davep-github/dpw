d2a4a65ce0175636b620f6e748d7342c51170872
David Panariti <dapanarx@intel.com>
2011-12-21 11:21:39 -0500

    moved more stuff to git.

---
+++ tilde/.go
@@ -0,0 +1,20 @@
+Ee|elm|			${HOME}/.elm/
+Ee|jobs|		${HOME}/jobs/
+Ee|pine|		${HOME}/pine/
+Ee|ppp|			${HOME}/.ppp/
+Ee|ulb|			/usr/local/bin/
+Ee|yokel|               ${HOME}/yokel/
+Ee|yb|			${yokel}bin/
+Ee|ysb|			${yokel}sbin/
+Ee|ye|			${yokel}etc/
+q|noway|		no-way-we-see-this-since-selector-is-q
+Ee|ksrc|		/slice5/usr/src/sys/
+Ee|pdb|			${HOME}/etc/pdb/
+Ee|pydb|		${HOME}/etc/pydb/
+Ee|doc|			${HOME}/doc/
+;|bin|                  should not be used, but following should.
+Ee|bin|			${HOME}/bin/
+Ee|lisp|		${HOME}/lisp/
+E|el|                   ${HOME}/lisp/
+;E|elisponly|           a test which should not match
+Ee|testA|testB|testC| 	value-for-testABC
