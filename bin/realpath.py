145351da31617de8d4d7004458c7b182639403c3
David Panariti <dapanarx@intel.com>
2012-01-13 16:17:50 -0500

    misc

--- bin/realpath.py
+++ bin/realpath.py
@@ -2,16 +2,26 @@
 
 import sys, os
 
-argv = sys.argv[1:]
-
-if argv[0] == "-r":
-    translator = os.path.relpath
-    argv = argv[1:]
-else:
+def main(argv):
+    import getopt
+    terminator = "\n"
+    opt_string = "zr"
     translator = os.path.realpath
+    opts, args = getopt.getopt(argv[1:], opt_string)
+    for o, v in opts:
+        if o == '-z':
+            # Handle opt
+            # for, say, xargs -0
+            terminator = "\000"
+            continue
+        if o == '-r':
+            translator = os.path.relpath
 
-for a in argv:
-    print translator(a)
+    for fileName in args:
+        rp = translator(fileName)
+        print '%s%s' % (rp, terminator),
 
-sys.exit(0)
+if __name__ == "__main__":
+    main(sys.argv)
+
 
