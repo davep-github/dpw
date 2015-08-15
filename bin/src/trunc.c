58f4ec8226e0ce1c00087cd31dc3f86603ffb7f0
David Panariti <davep.dpw@meduseld.net>
2015-08-15 18:39:23 -0400

    misc

--- bin/src/trunc.c
+++ bin/src/trunc.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
 main(
-int	argc,
-char	*argv[])
+    int	argc,
+    char	*argv[])
 {
-	long	to;
-	char	*p;
-	int	i;
-	int	rc = 0;
+    long	to;
+    char	*p;
+    int	i;
+    int	rc = 0;
 
-	to = strtoul(argv[1], &p, 0);
+    if (argc < 3) {
+        fprintf(stderr, "Usage: length file [file...]\n");
+        return 1;
+    }
+    
+    to = strtoul(argv[1], &p, 0);
 
-	for (i = 2; i < argc; i++)
-	{
-		if (truncate(argv[i], to) != 0)
-		{
-			fprintf(stderr, "trunc: cannot trunc: %s", argv[i]);
-			perror(" ");
-			rc = 1;
-		}
-	}
+    for (i = 2; i < argc; i++)
+    {
+        if (truncate(argv[i], to) != 0)
+        {
+            fprintf(stderr, "trunc: cannot trunc: %s", argv[i]);
+            perror(" ");
+            return 1;
+        }
+    }
 
-	exit(rc);
+    return rc;
 }
