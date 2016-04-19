4a2b941bdeb782969274f7c942f51ed6c15472ff
David Panariti <David.Panariti@amd.com>
2016-04-19 11:23:28 -0400

    Added ability to reverse sort, and a -h help option
    Signed-off-by: David Panariti <David.Panariti@amd.com>

--- bin/sort-by-len.py
+++ bin/sort-by-len.py
@@ -3,7 +3,11 @@
 import sys, os
 import dp_io
 
-def sort_by_len(strings):
+# Can I have used a sort arg to sort?  Doing this allows me to sort an
+# array based on the value of a property of the array members (e.g. string
+# len) rather than the values of the members.
+# It's already written and works, bone-headed as it may be.
+def arrange_by_len(strings):
     strings_by_len = {}
     for s in strings:
         s = s[:-1]
@@ -11,10 +15,10 @@ def sort_by_len(strings):
         strings_by_len[l] = strings_by_len.get(l,[]) + [s]
     return strings_by_len
 
-def sort_and_print_strings(strings, printor=dp_io.printf):
-    strings_by_len = sort_by_len(strings)
+def sort_and_print_strings(strings, reverse_sort_p, printor=dp_io.printf):
+    strings_by_len = arrange_by_len(strings)
     lengths = strings_by_len.keys()
-    lengths.sort()
+    lengths.sort(reverse=reverse_sort_p)
     for length in lengths:
         lines = strings_by_len[length]
         for l in lines:
@@ -28,27 +32,31 @@ def read_files(files):
         fob.close()
     return strings
     
-def sort_and_print_files(files):
-    sort_and_print_strings(read_files(files))
+def sort_and_print_files(files, reverse_sort_p):
+    sort_and_print_strings(read_files(files, reverse_sort_p))
 
-def sort_and_print_fobs(file_objects):
+def sort_and_print_fobs(file_objects, reverse_sort_p):
     strings = []
     for fob in file_objects:
         strings = strings + fob.readlines()
-    sort_and_print_strings(strings)
+    sort_and_print_strings(strings, reverse_sort_p)
 
 def main(argv):
     import getopt
-    opt_string = ""
+    opt_string = "hr"
+    reverse_sort_p = False
     opts, args = getopt.getopt(argv[1:], opt_string)
-    #for o, v in opts:
-    #    if o == '-<option-letter>':
-    #        # Handle opt
-    #        continue
+    for o, v in opts:
+        if o == '-h':
+            print "Sort input lines by length."
+            continue
+        if o == '-r':
+            reverse_sort_p = True
+            continue
     if args:
-        sort_and_print_files(args)
+        sort_and_print_files(args, reverse_sort_p)
     else:
-        sort_and_print_fobs([sys.stdin])
+        sort_and_print_fobs([sys.stdin], reverse_sort_p)
         
 
 if __name__ == "__main__":
