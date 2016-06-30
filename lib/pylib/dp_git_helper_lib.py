b3a25b66b02d36a48006800cc20292479ec26416
Just A. Guy <just.a.guy@meduseld.net>
2016-06-30 17:55:29 -0400

    New stuff.
    Signed-off-by: Just A. Guy <just.a.guy@meduseld.net>

---
+++ lib/pylib/dp_git_helper_lib.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os, sys
+import argparse
+import dp_io, dp_utils
+opath = os.path
+
+## Make error check finer grained.
+def git_current_branch(dir = None):
+    try:
+        old_cwd = None
+        if dir:
+            old_cwd = dp_utils.realpwd()
+            os.chdir(dir)
+
+        gcb = dp_utils.bq("git rev-parse --abbrev-ref=strict HEAD")
+
+        if old_cwd:
+            os.chdir(old_cwd)
+
+        return gcb
+    except:
+        return False
+
+    
+    
