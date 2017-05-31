21880886698af88e4b473f01b01aba7392361a99
David Panariti <David.Panariti@amd.com>
2017-05-31 15:52:46 -0400

    Added code to delete. Fixed seriously broken prepend code. Code written and produced by Algernon.

--- bin/pathadd2_lib.py
+++ bin/pathadd2_lib.py
@@ -10,6 +10,8 @@ def main(nargv):
     isep = ":"
     osep = isep
     prepend_p = False
+    delete_p = False
+
     argi = 1
     nargs = len(nargv)
     while argi < nargs:
@@ -20,6 +22,8 @@ def main(nargv):
         argi += 1
         if a == '-p':
             prepend_p = True
+        elif a == '-d':
+            delete_p = True
         elif a == '-s':
             isep = nargv[argi]
             osep = isep
@@ -41,22 +45,32 @@ def main(nargv):
             print >>sys.stderr, "Bad arg>%s<" % (a)
     #print >>sys.stderr, "nargv[%d:]>%s<" % (argi, nargv[argi:])
 
-    if prepend_p:
-        car_elements = nargv[argi + 1:] # separate args
-        cdr_elements = nargv[argi].split(isep)  # path:like:arg
-    else:
-        car_elements = nargv[argi].split(isep)
-        cdr_elements = nargv[argi + 1:]
+    path_elements = nargv[argi].split(isep)
+    new_elements = nargv[argi + 1:]
 
-    for i in range(car_elements.count('')):
-        car_elements.remove('')
+    for i in range(new_elements.count('')):
+        new_elements.remove('')
 
-    for i in range(cdr_elements.count('')):
-        cdr_elements.remove('')
+    for i in range(path_elements.count('')):
+        path_elements.remove('')
+
+    #print "1:new_elements:", osep.join(new_elements)
+    #print "1:path_elements:", osep.join(path_elements)
 
     # remove duplicates of elements in car from cdr
-    for d in car_elements:
-        if d in cdr_elements:
-            cdr_elements.remove(d)
+    for d in new_elements:
+        while d in path_elements:
+            #print d, "in", path_elements
+            path_elements.remove(d)
+            #print d, "in", path_elements
+    #print "delete_p:", delete_p
+    #print "2:new_elements:", osep.join(new_elements)
+    #print "2:path_elements:", osep.join(path_elements)
 
-    print osep.join(car_elements + cdr_elements)
+    if delete_p:
+        elements = path_elements
+    elif prepend_p:
+        elements = new_elements + path_elements
+    else:
+        elements = path_elements + new_elements
+    print osep.join(elements)
