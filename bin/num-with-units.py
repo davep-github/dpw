5ec1485d35dc5a13cc424edc8ef1ef2bd710d8e0
David Panariti <David.Panariti@amd.com>
2017-10-09 17:43:12 -0400

    Still fucked up. Why do I do things so stupidly now?

--- bin/num-with-units.py
+++ bin/num-with-units.py
@@ -5,18 +5,24 @@ import dp_utils
 
 def main(argv):
     import getopt
-    opt_string = "2b"
-    base10_p = True
+    opt_string = "2bh"
+    powers_of_two_p = False
+    base = 10
     opts, args = getopt.getopt(argv[1:], opt_string)
     for o, v in opts:
         if o[1:] in "2b":
-            base10_p = False
+            powers_of_two_p = True
+            base = 2
+            continue
+        if o[1:] in "h":
+            powers_of_two_p = True
+            base = 16
             continue
 
     for arg in args:
         if arg == "2b":
             continue
-        print dp_utils.numWithUnits(arg, base10_p)
+        print dp_utils.numWithUnits(arg, powers_of_two_p, base=base)
 
 if __name__ == "__main__":
     main(sys.argv)
