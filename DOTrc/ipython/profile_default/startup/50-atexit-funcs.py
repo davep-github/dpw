3de21a604f3a17e3641a0def8e08f591e9569f8d
David Panariti <David.Panariti@amd.com>
2018-01-02 12:17:42 -0500

    Commented out experimental code.

--- DOTrc/ipython/profile_default/startup/50-atexit-funcs.py
+++ DOTrc/ipython/profile_default/startup/50-atexit-funcs.py
@@ -1,6 +1,26 @@
+import atexit
+#import os
+#import readline
+#import rlcompleter
+
 def goodbye(name, adjective):
     print 'Goodbye, %s, it was %s to meet you.' % (name, adjective)
 
-import atexit
 atexit.register(goodbye, 'Dave', 'ambiguous')
 
+# historyPath = os.path.expanduser("~/.pyhistory")
+
+#def save_history(historyPath=historyPath):
+#    import readline
+#    readline.write_history_file(historyPath)
+
+#def load_history(historyPath=historyPath):
+#    print "in load_history()>%s<" % (historyPath,)
+#    if os.path.exists(historyPath):
+#        print "loading..."
+#        readline.read_history_file(historyPath)
+
+#atexit.register(save_history)
+#load_history(historyPath)
+
+#del os, atexit, readline, rlcompleter, save_history, historyPath, load_history
