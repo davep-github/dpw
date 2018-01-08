29f21b8724308158ba58dbc8645f5dd652ec0d6a
David Panariti <David.Panariti@amd.com>
2018-01-08 10:55:46 -0500

    The 'macsen: Gotta keep 'em separated.

---
+++ lisp/fsf-custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(isearch-allow-scroll t)
+ '(isearch-resume-in-command-history t)
+ '(save-place t)
+ '(scroll-bar-mode (quote right))
+ '(show-paren-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
