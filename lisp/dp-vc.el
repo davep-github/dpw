016b142c4056daed38fe1f045271dd305e9c7d59
David Panariti <David.Panariti@amd.com>
2018-03-26 17:41:59 -0400

    Use this to handle various VC packages.

--- lisp/dp-vc.el
+++ lisp/dp-vc.el
@@ -1,4 +1,8 @@
 ;;
 ;; vc (not just vc.el) hacks, etc.
 ;;
-
+(defun dp-diff-mode-goto-vc-dir ()
+  (interactive)
+  ;; Fix this since there can be >1 dirs.
+  ;; ?Add switched-to-time as a BLV?
+  (switch-to-buffer-other-window "*vc-dir*"))
