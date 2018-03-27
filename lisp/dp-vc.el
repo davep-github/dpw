c9bd85a091105ab0ab7ce9dd5f7475ea998fe206
David Panariti <David.Panariti@amd.com>
2018-03-26 21:48:58 -0400

    Moved another function here.

--- lisp/dp-vc.el
+++ lisp/dp-vc.el
@@ -6,3 +6,12 @@
   ;; Fix this since there can be >1 dirs.
   ;; ?Add switched-to-time as a BLV?
   (switch-to-buffer-other-window "*vc-dir*"))
+
+(defadvice vc-diff (around dp-vc-advice activate)
+  (dp-push-window-config)
+  (dp-offer-to-start-editing-server)
+  ad-do-it
+  (local-set-key [(control ?c) (control ?c)] 
+                 'dp-kill-buffer-and-pop-window-config))
+
+(provide 'dp-vc)
