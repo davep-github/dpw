8e579fa5568aba3365a66eb7e4e6a5a22f295cef
David Panariti <David.Panariti@amd.com>
2018-04-04 22:11:47 -0400

    New, old, borrowed, bugged.

---
+++ lisp/contrib/xemacs.el-for-fsf-compat/igrep/_pkg.el
@@ -0,0 +1,5 @@
+;;;###autoload
+(package-provide 'igrep
+		 :version 1.16
+          :author-version "2.111"
+		 :type 'regular)
---
+++ lisp/contrib/xemacs.el-for-fsf-compat/igrep/custom-load.el
@@ -0,0 +1,10 @@
+;;; custom-load.el --- automatically extracted custom dependencies
+
+;;; Code:
+
+(autoload 'custom-add-loads "cus-load")
+
+(custom-add-loads 'igrep '("igrep"))
+(custom-add-loads 'compilation '("igrep"))
+
+;;; custom-load.el ends here
