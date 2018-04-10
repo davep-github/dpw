2afe3bd052c66a654d3f47f4ba37ae016fa29505
David Panariti <David.Panariti@amd.com>
2018-04-09 22:11:00 -0400

    New files.
    Signed-off-by: David Panariti <David.Panariti@amd.com>

---
+++ lisp/dp-adwaita0-theme.el
@@ -0,0 +1,33 @@
+(deftheme dp-adwaita0
+  "Created 2018-03-10.")
+
+(custom-theme-set-faces
+ 'dp-adwaita0
+ '(cursor ((((class color) (min-colors 89)) (:background "#00BBFF"))))
+ '(fringe ((t (:background "alice blue"))))
+ '(mode-line ((((class color) (min-colors 89)) (:box (:line-width -1 :style released-button) :background "white" :foreground "#2E3436"))))
+ '(mode-line-inactive ((((class color) (min-colors 89)) (:foreground "#C6C6C6" :background "white"))))
+ '(header-line ((((class color) (min-colors 89)) (:foreground "#CCCCCC" :background "black"))))
+ '(minibuffer-prompt ((((class color) (min-colors 89)) (:foreground "#0084C8" :bold t))))
+ '(region ((((class color) (min-colors 89)) (:background "#C2D5E9" :foreground unspecified))))
+ '(dired-header ((((class color) (min-colors 89)) (:bold t :foreground "#0084C8"))))
+ '(widget-button ((((class color) (min-colors 89)) (:bold t :foreground "#0084C8"))))
+ '(success ((((class color) (min-colors 89)) (:bold t :foreground "#4E9A06"))))
+ '(warning ((((class color) (min-colors 89)) (:foreground "#CE5C00"))))
+ '(error ((t (:foreground "#B50000"))))
+ '(font-lock-builtin-face ((((class color) (min-colors 89)) (:foreground "#A020F0"))))
+ '(font-lock-constant-face ((t (:foreground "#F5666D" :weight bold))))
+ '(font-lock-comment-face ((((class color) (min-colors 89)) (:foreground "#204A87"))))
+ '(font-lock-function-name-face ((((class color) (min-colors 89)) (:foreground "#00578E" :bold t))))
+ '(font-lock-keyword-face ((((class color) (min-colors 89)) (:bold t :foreground "#A52A2A"))))
+ '(font-lock-string-face ((((class color) (min-colors 89)) (:foreground "#4E9A06"))))
+ '(font-lock-type-face ((((class color) (min-colors 89)) (:foreground "#2F8B58" :bold t))))
+ '(font-lock-variable-name-face ((((class color) (min-colors 89)) (:foreground "#0084C8" :bold t))))
+ '(font-lock-warning-face ((((class color) (min-colors 89)) (:foreground "#F5666D" :bold t))))
+ '(link ((((class color) (min-colors 89)) (:underline t :foreground "#0066CC"))))
+ '(link-visited ((((class color) (min-colors 89)) (:underline t :foreground "#6799CC"))))
+ '(highlight ((t (:background "#4A90D9" :foreground "black"))))
+ '(isearch ((t (:background "blue1" :foreground "white"))))
+ '(default ((t (:background "#EDEDED" :foreground "black")))))
+
+(provide-theme 'dp-adwaita0)
