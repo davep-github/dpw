e4cd9b6daabac4f183247675801535367bd0ce5d
David Panariti <davep.git@meduseld.net>
2018-05-18 11:39:55 -0400

    Made changes to keymap & command in the rebase buffer.

--- lisp/dp-magit.el
+++ lisp/dp-magit.el
@@ -8,31 +8,97 @@
 (defun dp-magit-mode-setup-hook ()
   )
 
-(defun magit-log-format-margin (author date)
-  (-when-let (option (magit-margin-option))
-    (-let [(_ style width details details-width)
-           (or magit-buffer-margin
-               (symbol-value option))]
-      (magit-make-margin-overlay
-       (concat (and details
-                    (concat (propertize (truncate-string-to-width
-                                         (or author "")
-                                         details-width
-                                         nil ?\s (make-string 1 magit-ellipsis))
-                                        'face 'magit-log-author)
-                            " "))
-               (propertize
-                (if (stringp style)
-                    (format-time-string
-                     style
-                     (seconds-to-time (string-to-number date)))
-                  (-let* ((abbr (eq style 'age-abbreviated))
-                          ((cnt unit) (magit--age date abbr)))
-                    (format (format (if abbr "%%2i%%-%ic" "%%2i %%-%is")
-                                    (- (funcall width style details details-width)
-				       (if details (1+ details-width) 0)))
-                            cnt unit)))
-                'face 'magit-log-date))))))
+;; Nice convention to put key bindings for modes alone in a function.
+;; <clutch-pearls-on-soap-box>Wah! Name space pollution!</clutch-pearls-on-soap-box>
+;; Make modification and application easier.
+(defun dp-git-rebase-bind-keys ()
+  (dp-define-local-keys '(
+			  [(meta down)] other-window
+			  [(meta up)] dp-other-window-up
+			  [(shift down)] git-rebase-move-line-down
+			  [(shift up)] git-rebase-move-line-up
+			  ;; git-rebase-kill-lines does one line.
+			  ;; we do one by default, else does prefix-arg lines.
+			  [?k] dp-git-rebase-kill-lines
+			  ;; Buffer is RO, so
+			  ;; `dp-delete-to-end-of-line' can't be used,
+			  ;; so remap it.  We are essentially killing
+			  ;; the line.
+			  [(meta ?k)] dp-git-rebase-kill-lines
+			  )
+			))
+
+(defun dp-git-rebase-mode-hook ()
+  (dp-git-rebase-bind-keys)		; YAY! More keymaps broken.
+  )
+(add-hook 'git-rebase-mode-hook 'dp-git-rebase-mode-hook)
+
+(defun dp-git-rebase-kill-lines (&optional num-lines)
+  "Call `git-rebase-kill-lines' NUM-LINES times.
+If NUM-LINES:
+region active -- lines in region.
+eq '- -- all lines till EOB.
+= 9 -- all lines.
+else: NUM-LINES lines.
+"
+  (interactive "p")
+  (setq num-lines
+	(cond
+	 ((dp-region-active-p)
+	  (goto-char (car (dp-region-boundaries-ordered)))
+	  (dp-number-lines-region))
+	 ((numberp num-lines)
+	  (cond
+	   ((< num-lines 0)
+	    (count-lines (point) (point-max)))
+	   ((equal num-lines 0)
+	    (goto-char (point-min))
+	    (count-lines (point-min) (point-max)))
+	   (t num-lines)))
+	 (t
+	  (error "Can't determine NUM-LINES from arg>%s<" num-lines))
+	 ))
+
+  ;; Make sure rebase code doesn't move to the next line since it
+  ;; won't move if the line is already killed.
+  (let ((git-rebase-auto-advance nil))
+    (dotimes (line-num num-lines)
+      (git-rebase-kill-line)
+      (forward-line))))
+
+(defun dp-magit-hide-untracked ()
+  (interactive)
+  (save-excursion ; May not work depending on how the collapsing works.
+    (magit-jump-to-untracked)
+    (call-interactively 'magit-section-hide)))
+  
+;; This hack isn't needed at work.
+;; And now tis not need at home.
+;; (defun magit-log-format-margin (author date)
+;;   (-when-let (option (magit-margin-option))
+;;     (-let [(_ style width details details-width)
+;;            (or magit-buffer-margin
+;;                (symbol-value option))]
+;;       (magit-make-margin-overlay
+;;        (concat (and details
+;;                     (concat (propertize (truncate-string-to-width
+;;                                          (or author "")
+;;                                          details-width
+;;                                          nil ?\s (make-string 1 magit-ellipsis))
+;;                                         'face 'magit-log-author)
+;;                             " "))
+;;                (propertize
+;;                 (if (stringp style)
+;;                     (format-time-string
+;;                      style
+;;                      (seconds-to-time (string-to-number date)))
+;;                   (-let* ((abbr (eq style 'age-abbreviated))
+;;                           ((cnt unit) (magit--age date abbr)))
+;;                     (format (format (if abbr "%%2i%%-%ic" "%%2i %%-%is")
+;;                                     (- (funcall width style details details-width)
+;; 				       (if details (1+ details-width) 0)))
+;;                             cnt unit)))
+;;                 'face 'magit-log-date))))))
 
 (provide 'dp-magit)
 
