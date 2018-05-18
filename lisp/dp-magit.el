2c03818670e8d3878940a0edb2e742821d8e6042
David Panariti <David.Panariti@amd.com>
2018-05-17 21:49:07 -0400

    Added some rebase key bindings and some functions to rebase =i.
    Signed-off-by: David Panariti <David.Panariti@amd.com>

--- lisp/dp-magit.el
+++ lisp/dp-magit.el
@@ -1,103 +1,118 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(apropos-do-all t)
- '(auth-sources
-   (quote
-    ("secrets:Login" "~/.authinfo" "~/.authinfo.gpg" "~/.netrc")))
- '(beacon-blink-when-focused t)
- '(beacon-mode t)
- '(comint-buffer-maximum-size 8192)
- '(comint-input-autoexpand t)
- '(comint-input-ignoredups t)
- '(comint-input-ring-size 5120)
- '(comint-mode-hook (quote (dp-comint-mode-hook)))
- '(comint-move-point-for-output nil)
- '(comint-password-prompt-regexp
-   "\\(\\([Oo]ld \\|[Bb]ad \\|[Nn]ew \\|^\\)?[Pp]assword\\|pass[ _-]?phrase\\):?\\s-*\\'\\|\\(Enter passphrase for.*: \\)\\|\\(Password for .*\\)\\|\\(Bad passphrase, try again for.*:\\)\\|\\(Enter password.*:\\s-+\\)\\|\\(\\(\\[sudo\\] \\)?[Pp]assword for .*\\(davep\\||d?panariti?\\|dapanarx.*\\).*:? \\)")
- '(comint-scroll-show-maximum-output nil)
- '(comint-use-prompt-regexp t)
- '(comment-style (quote extra-line))
- '(custom-enabled-themes (quote (abyss)))
- '(custom-safe-themes
-   (quote
-    ("551596f9165514c617c99ad6ce13196d6e7caa7035cea92a0e143dbe7b28be0e" "64922fcf67155cb9138f9cd41730bc96dcace4076ea157a13764ea809712fade" "128ece6b395c75abec230985113714d7ec251419a56d3850d42e4e77ed57f919" "58ff81f7a8e74f1633c1ce4fb135112dc8736e5b240c9458d2fe43ce930bd76d" "9eb84b9c9c03c789e7d10c02a427d828de4a3a0075bdee039898fa425f43fcad" "28dcfcfad6f70f319aa7cf3a92afb78cc4ba3f614c063fee9cd095b2efb3e64e" "de11dfdc2b94c89baaca111b470bc2ef55b8c5f31627e2e7c682d0309ab611e0" "48d5e503e37a5587f3416e8f3de3015b2c0fb971c05e6f4fb3be7bb98ffa0f41" "c50f28265bdd44ce373f1a06367dfec66bc2adf6ee95d6513a7a07cbb3039c38" "935b1a2de5eb9c72ad904ecc7a607a4372d6808a9c1df73e39f578f76841aca9" "8a7ddbef5ae6addeb464e486c4e5c075caeb3c3bfd6d2eba4e59a556fcf11f14" "c76078b5340febca07edc1d54e54d01754b5e602f5d14332eb81244a7151bb5f" "2fb6c366aad4f6d78f75364a74be56735e8ddb2d0a60dc2c2220c89f17dd832e" "b04153b12fbb67935f6898f38eb985ec62511fd1df6e2262069efa8565874195" default)))
- '(display-time-mode t)
- '(dp-trailing-whitespace-use-trailing-ws-font-p t)
- '(echo-keystrokes 0.1)
- '(global-eldoc-mode nil)
- '(global-hl-line-mode nil)
- '(gmm-tool-bar-style (quote gnome))
- '(ibuffer-use-other-window nil)
- '(icomplete-mode t)
- '(icomplete-separator ",")
- '(ido-mode nil nil (ido))
- '(ido-use-filename-at-point (quote guess))
- '(isearch-allow-scroll t)
- '(isearch-lazy-highlight t)
- '(isearch-resume-in-command-history t)
- '(ispell-lazy-highlight nil)
- '(kill-do-not-save-duplicates t)
- '(kill-ring-max 128)
- '(lazy-highlight-initial-delay 0.1)
- '(lazy-highlight-interval 0)
- '(magit-auto-revert-mode nil)
- '(magit-commit-arguments (quote ("--signoff")))
- '(magit-pull-arguments (quote ("--rebase")))
- '(magit-rebase-arguments (quote ("--interactive")))
- '(message-send-mail-function (quote message-send-mail-with-sendmail))
- '(minibuffer-prompt-properties
-   (quote
-    (read-only t cursor-intangible t face minibuffer-prompt)))
- '(mouse-yank-at-point t)
- '(mu4e-attachment-dir "/home/dpanarit/Downloads")
- '(next-error-highlight t)
- '(next-error-highlight-no-select t)
- '(next-error-recenter (quote (4)))
- '(org-insert-mode-line-in-empty-file t)
- '(package-selected-packages
-   (quote
-    (abyss-theme nova-theme magit ecb thingopt escreen mew nhexl-mode mu-cite mu4e-maildirs-extension mu4e-jump-to-list mu4e-alert flycheck-cstyle flycheck-checkbashisms flymake-cppcheck flycheck-rust flycheck-pos-tip flycheck-cython flymake-python-pyflakes flycheck hyperbole diffview dired-du auto-overlays adjust-parens which-key sed-mode notes-mode on-screen bug-hunter beacon python pinentry metar diff-hl gited flylisp ggtags json-mode context-coloring)))
- '(query-replace-lazy-highlight nil)
- '(safe-local-variable-values
-   (quote
-    ((c-font-lock-extra-types "FILE" "bool" "language" "linebuffer" "fdesc" "node" "regexp")
-     (block-comment-end . "")
-     (folding-internal-margins)
-     (folded-file . t))))
- '(save-abbrevs nil)
- '(save-place-mode nil)
- '(scroll-bar-mode (quote right))
- '(search-whitespace-regexp nil)
- '(send-mail-function (quote smtpmail-send-it))
- '(sendmail-program "/home/dpanarit/bin/dp-msmtp")
- '(shell-cd-regexp "cd|chdir|g")
- '(shell-input-autoexpand t)
- '(shell-pushd-regexp "pushd|g")
- '(show-paren-mode t)
- '(show-trailing-whitespace nil)
- '(smtpmail-debug-info t)
- '(smtpmail-debug-verb t)
- '(smtpmail-local-domain nil)
- '(smtpmail-queue-index-file "sendmail-q-index")
- '(smtpmail-smtp-service 25)
- '(smtpmail-smtp-user nil)
- '(smtpmail-stream-type nil)
- '(tags-revert-without-query t)
- '(timeclock-mode-line-display nil)
- '(tool-bar-mode nil)
- '(wdired-allow-to-change-permissions t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(beacon-fallback-background ((t (:background "DodgerBlue3"))))
- '(dp-trailing-whitespace-face ((t (:background "light coral" :weight normal))))
- '(rectangle-preview ((t (:inherit region)))))
+;;
+;; We need to hack a function, so let's isolate all the magit stuff.
+
+(message "load dp-magit()...")
+
+(require 'magit)
+
+(defun dp-magit-mode-setup-hook ()
+  )
+
+(defun dp-magit-mode-hook ()
+  (interactive)
+  (dp-local-set-keys
+   '([?=] magit-diff-dwim)))
+
+;; Nice convention to put key bindings for modes alone in a function.
+;; <clutch-pearls-on-soap-box>Wah! Name space pollution!</clutch-pearls-on-soap-box>
+;; Make modification and application easier.
+(defun dp-git-rebase-bind-keys ()
+  (dp-define-local-keys '(
+			  [(meta down)] other-window
+			  [(meta up)] dp-other-window-up
+			  [(shift down)] git-rebase-move-line-down
+			  [(shift up)] git-rebase-move-line-up
+			  ;; git-rebase-kill-lines does one line.
+			  ;; we do one by default, else does prefix-arg lines.
+			  [?k] dp-git-rebase-kill-lines
+			  ;; Buffer is RO, so
+			  ;; `dp-delete-to-end-of-line' can't be used,
+			  ;; so remap it.  We are essentially killing
+			  ;; the line.
+			  [(meta ?k)] dp-git-rebase-kill-lines
+			  )
+			))
+
+(add-hook 'magit-mode-hook 'dp-magit-mode-hook)
+
+(defun dp-git-rebase-mode-hook ()
+  (dp-git-rebase-bind-keys)		; YAY! More keymaps broken.
+  )
+(add-hook 'git-rebase-mode-hook 'dp-git-rebase-mode-hook)
+
+;;
+;; Sometimes the original version works.
+;; Dunno why.
+;; Repo state/contents?
+;; version.
+;;
+(defun dp-git-rebase-kill-lines (&optional num-lines)
+  "Call `git-rebase-kill-lines' NUM-LINES times.
+If NUM-LINES:
+region active -- lines in region.
+eq '- -- all lines till EOB.
+= 9 -- all lines.
+else: NUM-LINES lines.
+"
+  (interactive "p")
+  (setq num-lines
+	(cond
+	 ((dp-region-active-p)
+	  (goto-char (car (dp-region-boundaries-ordered)))
+	  (dp-number-lines-region))
+	 ((numberp num-lines)
+	  (cond
+	   ((< num-lines 0)
+	    (count-lines (point) (point-max)))
+	   ((equal num-lines 0)
+	    (goto-char (point-min))
+	    (count-lines (point-min) (point-max)))
+	   (t num-lines)))
+	 (t
+	  (error "Can't determine NUM-LINES from arg>%s<" num-lines))
+	 ))
+
+  ;; Make sure rebase code doesn't move to the next line since it
+  ;; won't move if the line is already killed.
+  (let ((git-rebase-auto-advance nil))
+    (dotimes (line-num num-lines)
+      (git-rebase-kill-line)
+      (forward-line))))
+
+(defun dp-magit-hide-untracked ()
+  (interactive)
+  (save-excursion ; May not work depending on how the collapsing works.
+    (magit-jump-to-untracked)
+    (call-interactively 'magit-section-hide)))
+
+;; This hack isn't needed at work.
+;; And now tis not need at home.
+;; (defun magit-log-format-margin (author date)
+;;   (-when-let (option (magit-margin-option))
+;;     (-let [(_ style width details details-width)
+;;            (or magit-buffer-margin
+;;                (symbol-value option))]
+;;       (magit-make-margin-overlay
+;;        (concat (and details
+;;                     (concat (propertize (truncate-string-to-width
+;;                                          (or author "")
+;;                                          details-width
+;;                                          nil ?\s (make-string 1 magit-ellipsis))
+;;                                         'face 'magit-log-author)
+;;                             " "))
+;;                (propertize
+;;                 (if (stringp style)
+;;                     (format-time-string
+;;                      style
+;;                      (seconds-to-time (string-to-number date)))
+;;                   (-let* ((abbr (eq style 'age-abbreviated))
+;;                           ((cnt unit) (magit--age date abbr)))
+;;                     (format (format (if abbr "%%2i%%-%ic" "%%2i %%-%is")
+;;                                     (- (funcall width style details details-width)
+;; 				       (if details (1+ details-width) 0)))
+;;                             cnt unit)))
+;;                 'face 'magit-log-date))))))
+
+(provide 'dp-magit)
+
+(message "load dp-magit()... done.")
