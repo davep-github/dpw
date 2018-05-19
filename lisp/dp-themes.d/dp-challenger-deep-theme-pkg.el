d5920dab86e56e31809870c485839484d0a5030a
David Panariti <davep.git@meduseld.net>
2018-05-19 19:14:08 -0400

    Created custom theme dir and copied a theme there for to modify.
    Yay!  Let the thematic juices flow!

---
+++ lisp/dp-themes.d/dp-challenger-deep-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "challenger-deep-theme" "20180223.644" "challenger-deep Theme" '((emacs "24")) :commit "4315c762a53c86ecc8bbbd0513f98d591801a12c" :url "https://github.com/challenger-deep-theme/emacs")
