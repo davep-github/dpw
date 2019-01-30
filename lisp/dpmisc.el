110718d5bc6b8770b87e0e62f8700b55bfc41f5d
Dav P <davep.git@meduseld.net>
2019-01-30 17:33:51 -0500

    Many collisions & conflicts oh my.

--- lisp/dpmisc.el
+++ lisp/dpmisc.el
@@ -6109,10 +6109,12 @@ When beginning a sequence, (point) is saved.  This can be pushed onto
   "Make an extent.  Give it a property of ID-PROP for easy identification.
 Also give it the property 'dp-extent-p with value t.
 In addition, add the PLIST from PROPS to the extent."
-  (let ((extent (make-extent from to buffer-or-string))
-	(prop-list (append (list id-prop t
-                                 'dp-extent-id id-prop
-                                 'dp-extent-p t)
+  (let ((extent (make-extent (or from (point-min))
+			     (or to (point-max))
+			     buffer-or-string))
+	(prop-list (append (list id-prop t 
+                                 'dp-extent-id id-prop 
+                                 'dp-extent-p t) 
                            props)))
     ;; put a unique property on every extent we make for easy,
     ;; positive identification of all of our extents
