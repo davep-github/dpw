ca83b7518015deaf34e3bf5d463bd313af5ab9f9
Dav P <davep.git@meduseld.net>
2019-03-13 15:29:13 -0400

    Added mingus-stays-home.

--- lisp/dp-mingus.el
+++ lisp/dp-mingus.el
@@ -1,5 +1,20 @@
 (dmessage "dp-mingus loading...")
 
+(if (not (bound-and-true-p dp-wants-mingus-stay-at-home-p))
+    ;; Mingus.  Just mingus.
+    (autoload 'mingus-make-mode-line-help-echo "mingus" "\
+Make a string to use in the mode-line-help-echo for Mingus.
+
+\(fn)" t nil)
+  ;;
+  ;; Extra functionality when we're on the same machine as the mpd.
+  ;; E.g. access to the configuration and data directory.
+  ;;@todo XXX Should I revert to mingus.  Just mingus?
+  ;; Most of the code is for cd burning, which currently I don't use.  There is
+  ;; some kind of URL support (need to look at it) and some drag'n'drop code.
+  ;; Need to look at, too.
+  (autoload 'mingus "mingus-stays-home" nil t))
+
 (defun dp-mingus-random-album ()
   (interactive)
   (dp-mpd-random-album)
@@ -36,12 +51,14 @@
      [(up)] mingus-prev
      [(kp-down)] mingus-next
      [(kp-up)] mingus-prev
-     [?i] ,(kb-lambda (mingus-make-mode-line-help-echo))
+     [?i] ,(kb-lambda
+	     (message "%s" (mingus-make-mode-line-help-echo)))
      [?s] mingus-stop
      [?S] dp-mingus-do-play
      [(shift ?s)] mingus-play
      [?r] dp-mingus-random-album
-     )))
+     ))
+  (message "dp-mingus-player-bind-keys completed."))
 
 (defalias 'dp-music-player-bind-keys 'dp-mingus-player-bind-keys)
 
@@ -84,9 +101,10 @@
   (dp-show-trailing-whitespace -1)
   (dp-mingus-browse-mode-bind-keys))
 
-(add-hook 'mingus-browse-hook 'dp-mingus-browse-hook)
+(add-hook 'mingus-browse-hook 'dp-mingus-browse-mode-hook)
 
 (global-set-key [(control meta ?m)] 'mingus)
 (global-set-key [(control meta ?p)] 'dp-mingus-random-album)
 
+(provide 'dp-mingus)
 (dmessage "...dp-mingus loaded.")
