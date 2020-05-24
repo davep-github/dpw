0134bc6a9e32ccdf2bcb7c0f96445507df8c66ed
Dav P <davep.git@meduseld.net>
2020-05-24 10:16:56 -0400

    Wrote the conditionally defined hook; mostly key bindings.

--- lisp/dp-elpy.el
+++ lisp/dp-elpy.el
@@ -1,6 +1,23 @@
 ;;;
 ;;; Setup the Emacs Lisp PYthon development environment.
-(elpy-enable)
+;;;
+
+(when (bound-and-true-p dp-use-elpy-mode-p)
+  (elpy-enable)
+
+  (defun dp-elpy-mode-hook ()
+    (interactive)
+    ;; I wanted to do this in the python-mode hook but I had to use
+    ;; elpy-mode-map directly which I didn't like.
+    (define-key elpy-mode-map [(meta up)] 'dp-other-window-up)
+    (define-key elpy-mode-map [(meta down)] 'other-window)
+    (define-key elpy-mode-map [(control up)] 'dp-scroll-down)
+    (define-key elpy-mode-map [(control down)] 'dp-scroll-up)
+    )
+  (add-hook 'elpy-mode-hook 'dp-elpy-mode-hook)
+
+  (message "Completed dp-elpy.el setup.")
+  )
 
 (provide 'dp-elpy)
 
