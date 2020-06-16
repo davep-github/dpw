f2168ef069fbeeca59426afbb8ec4d7aae0402c9
Sound Chaser <davep.git@meduseld.net>
2020-06-16 19:10:56 -0400

    Some new misc stuff hanging out forever.

---
+++ bin/templates/python2-template.py
@@ -0,0 +1,76 @@
+
+#
+# davep's standard new Python file template.
+#
+
+import os, sys, errno
+import argparse
+import dp_io
+
+debug_file = sys.stderr
+verbose_file = sys.stderr
+warning_file = sys.stderr
+
+BROKEN_PIPE_RC = 1
+IOERROR_RC = 1
+
+##e.g. class App_arg_action(argparse.Action):
+##e.g.     def __call__(self, parser, namespace, values, option_string=None):
+##e.g.         regexps = getattr(namespace, self.dest)
+##e.g.         regexps.append(values)
+##e.g.         setattr(namespace, self.dest, regexps)
+##e.g.         setattr(namespace, "highlight_grep_matches_p", True) 
+
+def main(argv):
+
+    oparser = argparse.ArgumentParser()
+    oparser.add_argument("--debug", "--dl",
+                         dest="debug_level",
+                         type=int,
+                         default=-1,
+                         help="Set debug level. Use with, e.g. "
+                         "dp_io.cdebug(<n>, fmt [, ...])")
+    oparser.add_argument("--verbose-level", "--vl",
+                         dest="verbose_level",
+                         type=int,
+                         default=-1,
+                         help="Set verbose/trace level. Use with, e.g. "
+                         "dp_io.ctracef(<n>, fmt [, ...])")
+    oparser.add_argument("-q", "--quiet",
+                         dest="quiet_p",
+                         default=False,
+                         action="store_true",
+                         help="Do not print informative messages.")
+##e.g.     oparser.add_argument("--app-action", "--aa",
+##e.g.                          dest="app_action_stuff", default=[],
+##e.g.                          action=App_arg_action,
+##e.g.                          help="Something normal actions can't handle.")
+
+    # ...
+
+    # For non-option args, first arg is name that goes into app_args.
+    oparser.add_argument("non_option_args_like_file_names, etc.", nargs="*")
+
+    app_args = oparser.parse_args()
+    if app_args.quiet_p:
+        print "I am being quiet."
+    if app_args.debug_level >= 0:
+        dp_io.set_debug_level(app_args.debug_level, enable_debugging_p=True)
+    if app_args.verbose_level > 0:
+        dp_io.set_verbose_level(app_args.verbose_level, enable_debugging_p=True)
+
+
+if __name__ == "__main__":
+    # try:... except: nice for filters.
+    try:
+        sys.exit(main(sys.argv))
+    except IOError, e:
+        # We're quite often a filter reading or writing to a pipe.
+        if e.errno == errno.EPIPE:
+            # Ya see, the colon looks like a broken pipe, heh, heh.
+            # : |
+            print >>sys.stderr, ":Broken PIPE:"
+            sys.exit(BROKEN_PIPE_RC)
+        print >>sys.stderr, "IOError>%s<" % (e,)
+        sys.exit(IOERROR_RC)
+
---
+++ lisp/dp-filladapt.el
@@ -0,0 +1,45 @@
+(dmessage "Loading dp-filladapt...")
+
+;;; ********************
+;;; "Filladapt is a paragraph filling package.  When it is enabled it
+;;; makes filling (e.g. using M-q) much much smarter about paragraphs
+;;; that are indented and/or are set off with semicolons, dashes, etc."
+;;; (copped from sample.init.el)
+;;;
+;;; Give us a less powerful fallback if we can't find filladapt.
+(setq-default filladapt-mode t)
+(add-hook 'outline-mode-hook 'turn-off-filladapt-mode)
+(setq filladapt-mode-line-string " Fa")
+
+(defun dp-turn-on-auto-fill ()
+  (if (fboundp 'turn-on-filladapt-mode)
+      (turn-on-filladapt-mode))
+  (turn-on-auto-fill))
+
+(defun dp-turn-off-auto-fill ()
+  (if (fboundp 'turn-off-filladapt-mode)
+      (turn-off-filladapt-mode))
+  (auto-fill-mode 0))
+
+(defvar dp-filladapt-state-stack nil
+  "Stack of filladapt on/off statii")
+
+(defun dp-push-filladapt-state (on-p)
+  (setq dp-filladapt-state-stack
+	(cons filladapt-mode dp-filladapt-state-stack))
+  (if on-p
+      (turn-on-filladapt-mode)
+    (turn-off-filladapt-mode)))
+
+(defun dp-pop-filladapt-state ()
+  (let ((on-p (car dp-filladapt-state-stack)))
+    (setq dp-filladapt-state-stack
+	  (cdr dp-filladapt-state-stack))
+    (if on-p
+	(turn-on-filladapt-mode)
+      (turn-off-filladapt-mode))))
+
+
+(provide 'dp-filladapt)
+
+(dmessage "Loading dp-filladapt...done")
