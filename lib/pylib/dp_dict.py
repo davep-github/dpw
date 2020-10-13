18df2f1247bbd5331055ba88a7e6b12c725d70a9
Sound Chaser <davep.git@meduseld.net>
2020-10-13 13:02:59 -0400

    Set a bunch of options and there're a lot more to look at.
    One important one is that tab completion will do a wild job looking for
    stuff. E.g. no files start with q.  However obsequious does.
    $ q<tab> expands to obsequious because it has a q.
    *NEED* to fix this.

--- lib/pylib/dp_dict.py
+++ lib/pylib/dp_dict.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
-### Time-stamp: <08/07/29 17:53:10 davep>
+### Time-stamp: <2020-10-09 17:59:47 davep>
 #############################################################################
 ## @package 
 ##
 import sys, os
-import UserDict
+import collections
 
-class DP_UserDict(UserDict.UserDict):
+UserDict = collections.UserDict
+class DP_UserDict(UserDict):
     def __init__(self, *args, **kw_args):
         ## Pretty transparent
-        UserDict.UserDict.__init__(self, *args, **kw_args)
+        super().__init__(self, *args, **kw_args)
 
 ##     def __setattr__(self, name, value):
-##         UserDict.UserDict.__setattr__(self, name, value)        
+##         UserDict.UserDict.__setattr__(self, name, value)
 
     def __getattr__(self, name):
-        return UserDict.UserDict.get(name)
-        
-dpu = DP_UserDict()    
+        return UserDict.get(name)
+
+    def can_u_c_me(self):
+        print("Well, can you?")
+
+dpu = DP_UserDict()
